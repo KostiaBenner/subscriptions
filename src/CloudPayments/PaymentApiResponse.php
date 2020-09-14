b795b673f4b17da51b2998fd4f98feb28a49d598
Sergey Nikiforov <ser.nikiforov@gmail.com>
2020-09-14 19:53:47 +0200

    error message fixes

--- src/CloudPayments/PaymentApiResponse.php
+++ src/CloudPayments/PaymentApiResponse.php
@@ -7,38 +7,38 @@ use Nikservik\Subscriptions\CloudPayments\ApiResponse;
 class PaymentApiResponse extends ApiResponse
 {
     protected $errorCodes = [
-        5051 => 'insufficient-funds',
-        5082 => 'incorrect-cvv',
-        5036 => 'restricted-card',
-        5062 => 'restricted-card',
-        5014 => 'incorrect-card',
-        5015 => 'incorrect-card',
-        5006 => 'incorrect-card',
-        5054 => 'incorrect-card',
-        5001 => 'contact-issuer',
-        5003 => 'contact-issuer',
-        5004 => 'contact-issuer',
-        5005 => 'contact-issuer',
-        5007 => 'contact-issuer',
-        5019 => 'contact-issuer',
-        5033 => 'contact-issuer',
-        5034 => 'contact-issuer',
-        5041 => 'contact-issuer',
-        5043 => 'contact-issuer',
-        5204 => 'contact-issuer',
-        5206 => 'contact-issuer',
-        5207 => 'contact-issuer',
-        5057 => 'contact-issuer',
-        5065 => 'contact-issuer',
-        5013 => 'try-later',
-        5030 => 'try-later',
-        5096 => 'try-later',
-        5091 => 'try-later-or-use-other',
-        5092 => 'try-later-or-use-other',
-        5063 => 'use-other',
-        5300 => 'use-other',
-        5031 => 'use-other',
-        5012 => 'use-other',
+        5051 => 'insufficient_funds',
+        5082 => 'incorrect_cvv',
+        5036 => 'restricted_card',
+        5062 => 'restricted_card',
+        5014 => 'incorrect_card',
+        5015 => 'incorrect_card',
+        5006 => 'incorrect_card',
+        5054 => 'incorrect_card',
+        5001 => 'contact_issuer',
+        5003 => 'contact_issuer',
+        5004 => 'contact_issuer',
+        5005 => 'contact_issuer',
+        5007 => 'contact_issuer',
+        5019 => 'contact_issuer',
+        5033 => 'contact_issuer',
+        5034 => 'contact_issuer',
+        5041 => 'contact_issuer',
+        5043 => 'contact_issuer',
+        5204 => 'contact_issuer',
+        5206 => 'contact_issuer',
+        5207 => 'contact_issuer',
+        5057 => 'contact_issuer',
+        5065 => 'contact_issuer',
+        5013 => 'try_later',
+        5030 => 'try_later',
+        5096 => 'try_later',
+        5091 => 'try_later_or_use_other',
+        5092 => 'try_later_or_use_other',
+        5063 => 'use_other',
+        5300 => 'use_other',
+        5031 => 'use_other',
+        5012 => 'use_other',
     ];
 
     public function isSuccessful()
@@ -49,12 +49,10 @@ class PaymentApiResponse extends ApiResponse
 
     public function getErrorMessage()
     {
-        if (! $this->Model)
+        if (! $this->Model || ! array_key_exists($this->ReasonCode, $this->errorCodes))
             return 'errors.undefined';
 
-        return array_key_exists($this->ReasonCode, $this->errorCodes) 
-            ? 'errors.'.$this->errorCodes[$this->ReasonCode]
-            : 'errors.'.$this->ReasonCode;
+        return 'errors.'.$this->errorCodes[$this->ReasonCode];
     }
 
     public function need3dSecure()
