53b578a115123caa852bee3a009108c707fef29c
Sergey Nikiforov <ser.nikiforov@gmail.com>
2020-10-29 18:27:23 +0100

    support messages in admin user view

--- views/admin/users/show.blade.php
+++ views/admin/users/show.blade.php
@@ -37,6 +37,36 @@
 
 <p class="my-4 mx-10">@lang('subscriptions::admin/users.role'): @lang('app.role'.$user->role)</p>
 
+<h2 class="sub-title">@lang('subscriptions::admin/users.supportMessages')</h2>
+
+        <div class="flex my-4 mx-10">
+            <div class="mr-3 relative w-12 h-12 text-gray-500">
+                <svg class="fill-current inline-block w-12 h-12" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="48"><path d="M6 14H4a2 2 0 0 1-2-2V4c0-1.1.9-2 2-2h12a2 2 0 0 1 2 2v2h2a2 2 0 0 1 2 2v13a1 1 0 0 1-1.7.7L16.58 18H8a2 2 0 0 1-2-2v-2zm0-2V8c0-1.1.9-2 2-2h8V4H4v8h2zm14-4H8v8h9a1 1 0 0 1 .7.3l2.3 2.29V8z"/></svg>
+                @if($user->unreadSupportMessagesCount('user') > 0)
+                    <div class="absolute text-sm top-0 right-0 mt-1">
+                        <div class="text-red-600">●</div> 
+                    </div>
+                    <div class="absolute text-gray-700 top-0 left-0 w-12 mt-3 ml-1 text-center">
+                        {{ $user->unreadSupportMessagesCount('user') }}
+                    </div>
+                @endif
+            </div>
+            <div class="">
+                @if($user->support_messages()->count() > 0)
+                    <div class="text-sm mt-1">
+                        @lang('subscriptions::admin/users.lastMessage')
+                    </div>
+                    <div class="text-sm mt-1 hover:no-underline">
+                        <a href="/support/dialog/{{ $user->id }}#read" class="hover:no-underline">
+                            {!! $user->support_messages()->orderBy('created_at', 'DESC')->first()->message !!}
+                        </a>
+                    </div>
+                @else
+                    <a href="/support/dialog/{{ $user->id }}#read"><b>@lang('subscriptions::admin/users.openDialog')</b></a>
+                @endif
+            </div>
+        </div>
+
 
 <h2 class="sub-title">@lang('subscriptions::admin/users.subscription')</h2>
 
